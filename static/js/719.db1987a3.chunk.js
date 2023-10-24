8e8dc8e8b91dfe4b5de8ec913152162af873accb
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-10-24 11:00:38 +0000

    Deploying to gh-pages from @ NikoNikoUA/shop-app@8f0177349151fb4800e719f7364abf08adc486d2 🚀

---
+++ static/js/719.db1987a3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[719],{719:function(e,t,a){a.r(t);var o=a(87),r=a(184);t.default=function(){return(0,r.jsxs)("div",{children:["Page is not found. Please go back to ",(0,r.jsx)(o.rU,{to:"/",children:"Home page"})]})}}}]);
+//# sourceMappingURL=719.db1987a3.chunk.js.map
